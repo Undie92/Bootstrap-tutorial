ebf12d20b08e78a51da118372caab2de65579d01
Undie92 <Undie92@hotmail.com>
2023-02-16 10:15:07 +0000

    fixed bug, repo showing without user entered

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -36,6 +36,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -61,9 +63,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
-$.when(
-    // we have got a response from the GitHub API
-).then(
-    // function to display it in the gh-user-data div
-    // unless we get an error!
-)
+
+$(document).ready(fetchGitHubInformation);
